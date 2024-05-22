272c9da951a3dd4782d8e2109cea1c3c64855cdb
adnanbezerra <adnanbezerra@proton.me>
2024-05-22 10:48:41 -0300

    refac/send catalogo to shared folder

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -1,47 +1,6 @@
-export function MainPage() {
-    const catalogo = [
-        {
-            nome: 'Mercedes AMG GT Azul',
-            img: 'mercedes1.jpg',
-            preco: '5'
-        },
-        {
-            nome: 'Mercedes SLS Preta',
-            img: 'mercedes2.jpg',
-            preco: '10'
-        },
-        {
-            nome: 'Mercedes SLK Preta',
-            img: 'mercedes3.jpg',
-            preco: '3'
-        },
-        {
-            nome: 'Mercedes 250S Vermelho',
-            img: 'mercedes4.jpg',
-            preco: '20'
-        },
-        {
-            nome: 'Mercedes Uno Branco',
-            img: 'mercedes5.jpg',
-            preco: '2,50'
-        },
-        {
-            nome: 'Mercedes E320 Preto',
-            img: 'mercedes6.jpg',
-            preco: '5'
-        },
-        {
-            nome: 'Mercedes-Benz S550 lwb black',
-            img: 'mercedes7.jpg',
-            preco: '10'
-        },
-        {
-            nome: 'Mercedes A38 AMG',
-            img: 'mercedes8.jpg',
-            preco: '1'
-        }
-    ];
+import { catalogo } from "../shared/files";
 
+export function MainPage() {
     function renderCatalogo() {
         let catalogoHtml = '';
 
